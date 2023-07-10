f9359607d9c55b8dfe754a9c1029331ebdca1aeb
tiemah <doreentiema87@gmail.com>
2023-07-10 16:52:26 +0300

    removed conn.php from gitignore

---
+++ conn.php
@@ -0,0 +1,8 @@
+<?php
+    define("HOST", "localhost");
+    define("USERNAME", "root");
+    define("PASSWORD", "");
+    define("DATABASE", "inventory");
+
+    $conn = mysqli_connect(HOST, USERNAME, PASSWORD, DATABASE);
+?>
